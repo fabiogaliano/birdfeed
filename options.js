3bcc106d592b076a59f1a7e25c2dd3413069d24d
Fábio Galiano <48589369+fabiogaliano@users.noreply.github.com>
2026-04-21 01:11:51 +0100

    Release v4.23.1

--- options.js
+++ options.js
@@ -355,7 +355,7 @@ let $showBlueReplyFollowersCountLabel = /** @type {HTMLElement} */ (document.que
 //#region Utility functions
 function exportConfig() {
   let $a = document.createElement('a')
-  $a.download = `birdfeed-v${chrome.runtime.getManifest().version}.config.txt`
+  $a.download = `birdfeed-v4.23.1.config.txt`
   $a.href = URL.createObjectURL(new Blob([
     JSON.stringify(optionsConfig, null, 2)
   ], {type: 'text/plain'}))
